626ba9b4f1d684b6042a9c3494de69e97221c191
Minh Luong <ngminhluong30@gmail.com>
2024-03-10 20:32:48 +0700

    <feat>[luongnm]: add entity, integrate ui theme

---
+++ project-management-be/src/main/java/datn/backend/entities/PostEntity.java
@@ -0,0 +1,45 @@
+package datn.backend.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "POST")
+public class PostEntity {
+
+    @Id
+    @Column(name = "ID")
+    String id;
+
+    @Column(name = "OBJECT_ID")
+    String title;
+
+    @Column(name = "CONTENT")
+    String content;
+
+    @Column(name = "CREATE_USER_ID")
+    String createUserId;
+
+    @Column(name = "CREATE_TIME")
+    Date createTime;
+
+    @Column(name = "CREATE_USER_ID")
+    String updateUserId;
+
+    @Column(name = "UPDATE_TIME")
+    Date updateTime;
+
+    @Column(name = "ENABLED")
+    Integer enabled;
+}
