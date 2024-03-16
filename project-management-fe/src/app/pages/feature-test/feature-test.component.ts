4e9978fb0b175661995d19166fbdf996c3041584
Minh Luong <ngminhluong30@gmail.com>
2024-03-16 23:10:11 +0700

    ok

---
+++ project-management-fe/src/app/pages/feature-test/feature-test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './feature-test.component.html',
+  styleUrl: './feature-test.component.scss'
+})
+export class FeatureTestComponent {
+
+}
